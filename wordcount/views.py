671984e754ebc37b0b51aa1fae3307bb412d0836
ChoHyoungSeo <francesco.johs@gmail.com>
2019-06-07 13:45:24 +0900

    update: about

--- wordcount/views.py
+++ wordcount/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'wordcount/home.html')
+
+def about(request):
+    return render(request, 'wordcount/about.html')
     
